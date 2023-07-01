325c47e75aa93ca883ca994de2888c09b89327a6
Soyniko1076 <121513340+Soyniko1076@users.noreply.github.com>
2023-07-01 21:44:38 +0300

    starting

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode></React.StrictMode>
-);
+// import './index.css';
+import { Profile } from './components/Profile/Profile';
+ReactDOM.createRoot(document.getElementById('root')).render(<Profile />);
